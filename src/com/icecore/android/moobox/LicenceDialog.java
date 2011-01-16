0d50e4e2561ba9d935fa8cba681af4121536bf5e
Gaëtan de Villèle <gdevillele@gmail.com>
2011-01-16 17:53:15 +0100

    add LicenceDialog

---
+++ src/com/icecore/android/moobox/LicenceDialog.java
@@ -0,0 +1,63 @@
+/////////////////////////////////////////////////////////////////
+///
+///	@file			: LicenceDialog.java
+///	@superclass		: Dialog
+///	@description 	: Licence text in a Dialog
+///	@project		: MooBox
+///
+///	Copyright 2011 Gaëtan de Villèle
+///
+/////////////////////////////////////////////////////////////////
+/*
+	This file is part of Foobar.
+
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+package com.icecore.android.moobox;
+
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+
+
+
+public class LicenceDialog extends Dialog
+{
+
+	public LicenceDialog(Context context)
+	{
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.licence_dialog);
+	}
+	
+	
+
+}
+
+
+
+
+
+
+
